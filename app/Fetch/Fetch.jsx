4bf406ce514c56955f49f63ad51771af7add7080
FayzullaevUmid707 <dilnoraurazova7@gmail.com>
2024-07-02 19:54:03 +0500

    intial upload

---
+++ app/Fetch/Fetch.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './header.css'
+
+const Fetch = () => {
+  return (
+    <div>
+        <div className='section'>
+            <div className='section-item'>
+            <h1 className='h1'>𝗖𝗿𝗲𝗮𝘁𝗲 𝗮 𝗽𝗹𝗮𝗻</h1>
+            <p className='p1'>Build a subscription plan that best fits your needs. We offer an <br />
+             assortment of the best artisan coffees from around the globe <br />
+              delivered fresh to your door.</p>
+              </div>
+        <img className='img2' src="https://s3-alpha-sig.figma.com/img/d03d/3e12/918c58cec7ea37f8c8e6baa99455fb4a?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mqkrIRKLAtK2g9ks9th5LfZOuKBtGjKckmlGopp6cB09dz-XA9OnSuO2ZpuSXyJPiK92kvsEPh8OnCMxpluwHyzYM~7yYzvgPyK85dvuYWvLje2MfrdFDJovStImKnBdhgsk-eWuvNgPCiorg4tIAvLPxAMmN8KlEJpXVvgAp5XwjewqosTyztUHBJ4el5TsKTcXDrBwyybG19yVRND9fg92gtRtC6zdTaDbwQR~MyPoQEnSMK6KB3v-~zSP2S35~CddGnovQpRCCaTAGJN~FpfUtYeSiuFar~8VVzSpWaaDHVk9TTwOalXrxnyH6BH6h6Wg3IcpnaI0WniYjYIvEA__" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Fetch
